bcdb61db09d326dcd70a320392499c8c6e5f86ef
Todd Langford-Archer <toddpla@gmail.com>
2018-10-22 16:34:46 +0100

    1 hour of my life i'll never get back

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-    erb :index
+    'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
+
+  run! if app_file == $0
 end
--- spec/features/view_index_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'view index page' do
-  scenario 'shows the index page when the app loads' do
-    visit '/'
-    expect(page).to have_content('Hello world')
-  end
-end
