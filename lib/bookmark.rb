c8837905f610345690a4b4192be8db9227bedc14
Irina Baldwin <irinabaldwin@Irinas-MacBook-Air.local>
2018-10-23 14:29:35 +0100

    Install and use Tableplus

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 class Bookmark
   def self.all
-    con = PG.connect :dbname => 'bookmarks', :user => 'toddlangford-archer'
+    con = PG.connect :dbname => 'bookmark_manager'
     results = con.exec "SELECT * FROM bookmarks;"
     results.map { |bookmark| bookmark['url'] }
   end
